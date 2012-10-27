6c5af6bd2824570f00990af12968a6de894443c7
Arkadiusz Dzięgiel <admin@glorpen.pl>
2012-10-27 11:14:47 +0200

    support for fonts dir

--- lib/django-compass.rb
+++ lib/django-compass.rb
@@ -14,6 +14,7 @@ class DjangoCompass
     settings = RubyPython.import(ENV["DJANGO_SETTINGS_MODULE"])
     @resolver = RubyPython.import("djangocompass.ruby_resolver").resolver
     #RubyPython.stop
+    
   end
   
   class Importer < Sass::Importers::Base
--- lib/django-compass/patches/urls.rb
+++ lib/django-compass/patches/urls.rb
@@ -5,7 +5,25 @@ module Compass::SassExtensions::Functions::Urls
     def image_url(path, only_path = Sass::Script::Bool.new(false), cache_buster = Sass::Script::Bool.new(true))
       path = path.value
       path = DjangoCompass.resolver.image_url(path).to_s
-      clean_url(path)
+      
+      if only_path.to_bool
+        Sass::Script::String.new(clean_path(path))
+      else
+        clean_url(path)
+      end
+    end
+  end
+  module FontUrl
+    def font_url(path, only_path = Sass::Script::Bool.new(false))
+      path = path.value # get to the string value of the literal.
+      
+      path = DjangoCompass.resolver.font_url(path).to_s
+
+      if only_path.to_bool
+        Sass::Script::String.new(clean_path(path))
+      else
+        clean_url(path)
+      end
     end
   end
 end
