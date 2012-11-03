c6ca3fa1f8dc1882d9a1350c7861f53711a7fb18
Arkadiusz Dzięgiel <admin@glorpen.pl>
2012-11-03 09:45:24 +0100

    fix in path mangling

--- lib/django-compass.rb
+++ lib/django-compass.rb
@@ -16,23 +16,34 @@ class DjangoCompass
     def to_s()
       "DjangoCompass::Importer"
     end
+    
     def find(uri, options)
       f = DjangoCompass.resolver("find_scss", uri)
       
       if f
         f = f.to_s
         syntax = (f =~ /\.(s[ac]ss)$/) && $1.to_sym || :sass
-        opts = options.merge(:syntax => syntax)
+        opts = options.merge(:syntax => syntax, :importer => self, :filename => f)
         return Sass::Engine.new(open(f).read, opts)
       end
       
       nil
     end
+    
     def find_relative(uri, base, options)
       nil
     end
-    def mtime(uri, options)
+    
+    def mtime(name, options)
+      file, s = find_real_file(name)
+      File.mtime(file) if file
+    rescue Errno::ENOENT
       nil
+    end
+    
+    def key(name, options)
+      [self.class.name + ":" + File.dirname(File.expand_path(name)),
+        File.basename(name)]
     end
   end
 end
